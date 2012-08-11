1fd542298d92ff5a6fa141ee3fe6bf8f8335d9d6
Tom Jakubowski <tom@crystae.net>
2012-08-11 00:47:32 -0700

    remove IE support, simplify approach
    - IE8 should still work, but not going to bother verifying.
    
    - add 'centered' option, which wraps the modal in a table-cell
      and makes #barebox_wrap display table
    
    - add 'bodyClass' option to add a class to <body> when the
      modal is displayed
    
    - remove 'target' option
    
    - demo includes some responsive examples

--- jquery.barebox.js
+++ jquery.barebox.js
@@ -1,26 +1,37 @@
 (function($) {
     $.fn.barebox = function(options) {
         options = $.extend({
-            target: $(this).data('target')
+            centered: true,
+            bodyClass: 'noscroll'
         }, options);
 
         var body = $('body');
-        var target = $(options.target);
+        var target = $(this).data('target');
         var overlay = $('<div id="barebox_overlay">').appendTo(body);
 
         if (target.length < 1)
             target = $(this);
 
+        var mother = target.parent();
+
         overlay.click(function() {
-            $('body, html').css({'overflow': 'auto'});
+            $(target).prependTo(mother);
             $(target).hide();
+            body.removeClass(options.bodyClass);
             $(this).remove();
         });
 
-        $('body, html').css({'overflow': 'hidden'});
+        body.addClass(options.bodyClass);
 
         overlay.show();
+        target.appendTo(overlay);
         $(target).show();
+
+        if (options.centered === true) {
+            overlay.css('display', 'table');
+            target.wrap($('<div id="barebox_cell">').css({ display: 'table-cell', 'vertical-align': 'middle', 'text-align': 'center' }));
+            target.css('display', 'inline-block');
+        }
         return target;
     };
 })(jQuery);
