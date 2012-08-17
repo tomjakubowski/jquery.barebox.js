38db606f2c50c617ee8b0e44823c0971f6aeb9e2
Tom Jakubowski <tom@crystae.net>
2012-08-17 14:11:19 -0700

    add 'barebox_close' method
    call on the object you called .barebox() on to close it.

--- jquery.barebox.js
+++ jquery.barebox.js
@@ -19,12 +19,7 @@
             if (e.target !== this && (options.centered && e.target.id !== "barebox_cell"))
                 return;
 
-            $(target).trigger('barebox_close');
-
-            $(target).prependTo(mother);
-            $(target).hide();
-            body.removeClass(options.bodyClass);
-            $(this).remove();
+            target.barebox_close();
         });
 
         body.addClass(options.bodyClass);
@@ -32,13 +27,34 @@
         overlay.show();
         target.appendTo(overlay);
         target.show();
-        target.data('barebox_modal', true);
+        target.data({
+            bareboxed: true,
+            barebox_options: options,
+            barebox_mother: mother });
 
         if (options.centered === true) {
             overlay.css('display', 'table');
-            target.wrap($('<div id="barebox_cell">').css({ display: 'table-cell', 'vertical-align': 'middle', 'text-align': 'center' }));
+            target.wrap($('<div id="barebox_cell">').css({
+                display: 'table-cell',
+                'vertical-align': 'middle',
+                'text-align': 'center' }));
             target.css('display', 'inline-block');
         }
         return target;
+    },
+    'barebox_close': function() {
+        var target = $(this);
+        var overlay = $('#barebox_overlay');
+        var mother = target.data('barebox_mother');
+        var options = target.data('barebox_options');
+
+        if (target.data('bareboxed') !== true)
+            throw 'barebox_close() must only be called on an active barebox';
+
+        target.trigger('barebox_close');
+        target.prependTo(mother);
+        target.hide();
+        $('body').removeClass(options.bodyClass);
+        overlay.remove();
     }});
 })(jQuery);
